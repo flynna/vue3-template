2fa967136cd1ae859b201ee807cc3007574b23bd
zhanghelin <zhanghelin@sobey.com>
2023-10-25 16:11:04 +0800

    chore: 添加 vue、vueRouter 相关资源的自动导入配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+const autoImport = require('unplugin-auto-import/vite');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    autoImport({
+      imports: [
+        'vue',
+        {
+          'vue-router': ['useRouter', 'useRoute'],
+        },
+      ],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
