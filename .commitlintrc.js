7a92037865e6ca64e9ffcd1845c393eb1d3e9b15
zhanghelin <zhanghelin@sobey.com>
2023-10-25 14:48:05 +0800

    chore: 添加 husky 提交校验，添加 git-cz 提交时界面交互

---
+++ .commitlintrc.js
@@ -0,0 +1,4 @@
+// rules: https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/config-conventional/index.js
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+}
